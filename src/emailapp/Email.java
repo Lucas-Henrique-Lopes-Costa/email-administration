c5675c7a8796ec6ddcb85b78ac465553a936d894
LucasH2o <lucass20586@gmail.com>
2020-11-15 16:51:18 -0300

    Sistema Pronto

---
+++ src/emailapp/Email.java
@@ -0,0 +1,114 @@
+package emailapp;
+
+import java.util.Scanner;
+
+public class Email {
+	private String primeiroNome; // private => as pessoas não podem acesssar 
+	private String ultimoNome;
+	private String senha;
+	private String departamento;
+	private String email;
+	
+	private String emailAlternativo;
+	
+	private int numeroCaracter = 5;
+	private String empresa = "wep";
+	private int capacidadeCaixaEmail = 500;
+	
+	// Receber o primeiro nome e o último nome
+	public Email(String primeiroNome, String ultimoNome) {
+		this.primeiroNome = primeiroNome;
+		this.ultimoNome = ultimoNome;
+		System.out.println("Olá, " + this.primeiroNome + " " + this.ultimoNome + ".");
+		
+		// Chamando o method do Departamento
+		this.departamento = setDepartament();
+		System.out.println("\nOla, " + this.departamento + ". Tenha um Bom Dia!\n");
+		
+		// Chamando o method da Senha
+		this.senha = senhaAleatoria(numeroCaracter);
+		// System.out.println("Sua senha é: " + this.senha);
+		
+		// Gerando o email
+		email = primeiroNome.toUpperCase() + "." + ultimoNome.toUpperCase() + "@" + departamento + "-" + empresa + ".com";
+		// System.out.println("Seu email é: " + email);
+	}
+	
+	// Pergutar o Departamento
+	private String setDepartament() {
+		System.out.print("\nColoque seu Deparamento : \n1 Vendedas\n2 Desenvolvimento\n3 Contabilidade\n0 Nenhum\n\nQual o seu DEPARTAMENTO: ");
+		
+		Scanner in = new Scanner(System.in);
+		
+		int choice = in.nextInt();
+		
+		// tipos das escolhas
+		if (choice == 1) {
+			return "vendedor";
+		}
+		
+		if (choice == 2) {
+			return "desenvolvedor";
+		}
+		
+		if(choice == 3) {
+			return "contador";
+		}
+		
+		else {
+			return "";
+		}
+	}
+	
+	// Gerando um Senha aleatória
+	private String senhaAleatoria (int length) {
+		String setSenha = "ABCDEFHIJKLMNOPQRSTUVWXYZ0123456789@#$";
+		
+		char [] senha = new char[length];
+		
+		for(int i=0; i<length; i++) {
+			int aleatorio = (int) (Math.random() * setSenha.length());
+			senha[i] = setSenha.charAt(aleatorio);
+			// System.out.println(aleatorio);
+			// System.out.println(setSenha.charAt(aleatorio));
+		}
+		return new String(senha);
+	}
+	
+	
+	// Capacidade da caixa de email
+	public void setCapacidade (int capacidade) { // public void permite você atribuí-lo no arquivo EmailApp
+		this.capacidadeCaixaEmail = capacidade;
+	}
+	
+	public int pegarCapacidade() { // com o public você estará atribuindo o valor no EmailApp
+		return capacidadeCaixaEmail;
+	}
+	
+	// Email alternativo
+	public void setEmailAlternativo (String altEmail) {
+		this.emailAlternativo = altEmail;
+	}
+	
+	public String pegarEmailAlternativo() {
+		return emailAlternativo;
+	}
+	
+	// Mudar a senha
+	public void mudarSenha (String senha) {
+		this.senha = senha;
+	}
+	
+	public String pegarSenha() {
+		return senha;
+	}
+	
+	public String showInfo() {
+		return "Nome: " + primeiroNome + " " + ultimoNome +
+				"\nNome da Empresa: " + empresa +
+				"\nEmail: " + email +
+				"\nCapacidade do Email: " + capacidadeCaixaEmail + "mb";
+	}
+	
+	
+}
